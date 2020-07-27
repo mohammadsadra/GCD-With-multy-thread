3a7016777d0c86a13ec16ad3bbbfbb134040e21d
Mohammad Sadra <mohammadsadrahaeri@gmail.com>
2020-07-27 22:36:52 +0430

    version 1.0

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Channels;
+
+namespace first
+{
+    
+    class Program
+    {
+
+        static long GCD(long n1, long n2)
+        {
+            if (n2 == 0)
+            {
+                
+                Console.WriteLine("\n" + n1);
+                return n1;
+            }
+            else
+            {
+                return GCD(n2, n1 % n2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                
+
+                if (input == "END")
+                {
+                    Console.WriteLine("See you later :))");
+                    break;
+                }
+
+                var numbers = input.Split(" ");
+
+                var t = 
+                    new Thread(
+                        unused => GCD(long.Parse(numbers[0]), long.Parse(numbers[1]))
+                    );
+                
+                t.Start();
+                
+                
+                
+            }
+        }
+    }
+}
