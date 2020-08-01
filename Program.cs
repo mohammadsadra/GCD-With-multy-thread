64c9b2bf375f0173eee93520b34058652452eab8
Mohammad Sadra Haeri <43936083+mohammadsadra@users.noreply.github.com>
2020-08-01 17:02:33 +0430

    Full exeption handler added & Guide added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Threading;
 
 namespace first
@@ -7,23 +8,40 @@ namespace first
     class Program
     {
 
-        static long GCD(long n1, long n2, int len)
+        static long GCD(long n1, long n2, int len, long n3, long n4)
         {
-            if (n2 == 0)
+            try
             {
-                Thread.Sleep(len * 1000);
-                Console.WriteLine("\n" + n1);
-                return n1;
+                if (n2 == 0)
+                {
+                    Thread.Sleep(len * 1000);
+                    Console.WriteLine("\nGCD of " + n3 +" & "+ n4+ " is: "+n1);
+                    
+                    return n1;
+                }
+                else
+                {
+                    return GCD(n2, n1 % n2, len, n3, n4);
+                }
             }
-            else
+            catch (Exception e)
             {
-                return GCD(n2, n1 % n2, len);
+                Console.WriteLine("wrong input");
+                return 0;
             }
+           
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("GUIDE:");
+            Console.WriteLine("1.You must enter two integer number then this application will help you to compute GCD of these numbers");
+            Console.WriteLine("2.When you write -END-, application will be closed");
+            Console.WriteLine("Enjoy :))\n");
+            
             while (true)
             {
+                Console.WriteLine("Enter two numbers:");
+                
                 var input = Console.ReadLine();
                 if (string.IsNullOrWhiteSpace(input))
                 {
@@ -37,16 +55,36 @@ namespace first
                     break;
                 }
 
-                var numbers = input.Split(" ");
+                try
+                {
+                    var numbers = input.Split(" ");
+                    var a = long.Parse(numbers[0]) + long.Parse(numbers[1]);
+                    if (numbers[3] != null)
+                    {
+                        Console.WriteLine("Wrong input, Try again please...\n");
+                        continue;
+                    }
+                    
 
-                var t = 
+
+                }
+                catch (System.FormatException e)
+                {
+                    Console.WriteLine("Wrong input, Try again please...\n");
+                    continue;
+                    
+                }
+                catch (System.IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Wrong input, Try again please...\n");
+                    continue;
+                }
+                var numbers1 = input.Split(" ");
+                var t =
                     new Thread(
-                        unused => GCD(long.Parse(numbers[0]), long.Parse(numbers[1]), numbers[0].Length)
+                        unused => GCD(long.Parse(numbers1[0]), long.Parse(numbers1[1]), numbers1[0].Length, long.Parse(numbers1[0]), long.Parse(numbers1[1]))
                     );
-                
                 t.Start();
-                
-                
                 
             }
         }
